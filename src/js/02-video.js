fbcaca2ea34a4433cc0a6462697f1f9e3f497a52
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2022-12-26 00:03:18 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import Player from '@vimeo/player';
 
+console.log(Player);
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function() {
+    console.log('played the video!');
+});
