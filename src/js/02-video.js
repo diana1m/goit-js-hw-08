b37ab97ca86fb717e1929a8bfe829307a0c98655
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2022-12-27 20:53:07 +0200

    Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,28 @@
 import Player from '@vimeo/player';
-
-console.log(Player);
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const CURRENT_TIME = "videoplayer-current-time";
 
-player.on('play', function() {
-    console.log('played the video!');
+function timeUpdate(data) {
+    console.log(data.seconds);
+    localStorage.setItem(CURRENT_TIME, data.seconds);
+}
+
+player.on('timeupdate', throttle(timeUpdate, 1000));
+
+const currentTime = JSON.parse(localStorage.getItem(CURRENT_TIME));
+
+player.setCurrentTime(currentTime).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            seconds < 0 || seconds > 571.52;
+            break;
+
+        default:
+            break;
+    }
 });
